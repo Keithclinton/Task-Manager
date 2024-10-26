a31fc422dd796c43805390a0ab0b60f11e0051c2
keybet <keybet06@gmail.com>
2024-10-26 19:29:18 +0300

    Trying to push...

--- taskmanager/tasks/serializers.py
+++ taskmanager/tasks/serializers.py
@@ -4,4 +4,4 @@ from .models import Task
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = '__all__'  # Serializes all fields in the Task model
+        fields = '__all__'  # Serializes all fields in the Task model all
